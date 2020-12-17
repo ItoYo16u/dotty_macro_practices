576de2451d68c1876398b467d44f3df28ce7b4ff
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-12-17 22:23:17 +0900

    add comment

--- src/main/scala/json.scala
+++ src/main/scala/json.scala
@@ -10,12 +10,12 @@ import scala.quoted._
 
 
 trait ToJson[A] {
-  
+
     def   toJson(a:A) : String
 }
 
 object ToJson {
- 
+
   inline given ToJson[Int] {
       override def toJson(a:Int): String = s"json of $a"
   }
@@ -28,7 +28,7 @@ object ToJson {
     case  '[t *: ts] => Expr(Type.show[t]) :: extractLabelsAsString[ts]  // type.show[t]
     case '[Tuple$package.EmptyTuple] => Nil
   }
-  // erasedValue[T] returns T. Useful when retrive T inline. 
+  // erasedValue[T] returns T. Useful when retrive T inline.
   // when T is Person's mirrored elem types, (String,String,Int) is given.
   def summonAll[T : Type](using q: Quotes): List[Expr[ToJson[_]]] = Type.of[T] match {
     // summonInline used to search implicits inline.
@@ -42,22 +42,24 @@ object ToJson {
   }
   // derived is an entry point for companion objects generated from drives clause.
   // derived[A] means derived by [A] (?)
-  
+
   inline def toJsonForCaseClasses[T](body: (T)=> String):ToJson[T] = {
     new ToJson[T]{
         override def toJson(a:T):String = body(a)
       }
   }
+  // ドキュメントだとusing(Quotes)になっているが、 (using q:Quotes)にしないとエラーになる
   inline def derived[A: Type](using q:Quotes): Expr[ToJson[A]] = {
     import quotes.reflect._
     // `summon` is for TypeClass instance.(?)
     val expression: Expr[Mirror.Of[A]] = Expr.summon[Mirror.Of[A]].get
     expression match {
+        // 以下のようにExprに対してパターンマッチできる
         // A がcase class,case object等の場合
       case '{ $m: Mirror.ProductOf[A] {type MirroredElemLabels = elementLabels;type MirroredElemTypes = elementTypes}}=>
-        
+
         val elementResolver = summonAll[elementTypes]
-        
+
         val callback: (Expr[A])=> Expr[String] = (a)=> {
         val values = elementResolver.zipWithIndex.foldLeft(Expr(Nil) :Expr[List[String]]){ case (acc,(_,index))=>
           val label = '{$a.asInstanceOf[Product].productElementName(${Expr(index)})}
@@ -78,7 +80,7 @@ object ToJson {
 
 object Macro {
   extension [T](inline x:T){
-    inline def toJson(using jsonizer:ToJson[T]):String = jsonizer.toJson(x)  
+    inline def toJson(using jsonizer:ToJson[T]):String = jsonizer.toJson(x)
   }
 
   implicit inline def gen[T]:ToJson[T]= ${genImpl[T]}
@@ -91,6 +93,6 @@ object Macro {
 * references
 * - https://gist.github.com/fsarradin/178876f079f29aa1092a9326899043ef Mar 29,2019.
 * - https://stackoverflow.com/questions/62853337/how-to-access-parameter-list-of-case-class-in-a-dotty-macro Answered Jul 12,2020
-* - http://dotty.epfl.ch/docs/reference/contextual/derivation.html 
-* 
+* - http://dotty.epfl.ch/docs/reference/contextual/derivation.html
+*
 * */
